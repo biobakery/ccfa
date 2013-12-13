951a615fdf93d6a1dd30d4f907bfa52fe18c2e05
Randall Schwager <schwager@hsph.harvard.edu>
2013-12-13 11:53:01 -0500

    feature: make it easier to specify what model to validate

--- mibc/validate.py
+++ mibc/validate.py
@@ -4,7 +4,7 @@ import tests
 def validate(obj, **opts):
     test_class_name = repr(obj)+"_Test"
     test_class = getattr(tests, test_class_name)
-    validator = test_class(cheat_all_tests=True)
+    validator = test_class(base=obj, cheat_all_tests=True)
 
     for test_func in validator:
         try:
