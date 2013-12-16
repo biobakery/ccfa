14e23818c921e9a2a7260dc481fa91ea1720b8c7
kb201a <kb201a@gmail.com>
2013-12-16 14:10:28 -0500

    Made minor tweaks (with Randall's help) to the python code.
    Created (bash) wrapper script which is invoked from drupal
    and creates the python environnment and calls the python
    script which checks the data deposition area for the user.
    
    Created the python script which formats the output of the
    tests and outputs HTML which eventually gets displayed
    via drupal for the ccfa user.

--- mibc/settings.py
+++ mibc/settings.py
@@ -1,2 +1,4 @@
 
-c_repository_root = "/var/www/ccfa/data/files/users"
+c_repository_root = "/n/home04/kbayer/files/users"
+#c_repository_root = "/var/www/ccfa/data/files/users"
+#c_repository_root = "/aux/ccfa/upload"
--- mibc/util.py
+++ mibc/util.py
@@ -3,6 +3,12 @@ import os
 def deserialize_csv(file_handle):
     for line in file_handle:
         cols = line.split('\t')
+        if len(cols) < 2:
+            raise AttributeError(
+                "Improper formatting in file %s - only %d column(s) found." % (
+                    file_handle.name, len(cols))
+            )
+
         yield ( 
             cols[0], 
             [ col.strip() for col in cols[1].split(',') ] 
