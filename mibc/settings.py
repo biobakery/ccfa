df08e931d9d5b55076b6260c8c9f2cd8b1a8ff99
Randall Schwager <schwager@hsph.harvard.edu>
2013-12-18 13:30:51 -0500

    add settings for email feature

--- mibc/settings.py
+++ mibc/settings.py
@@ -1,4 +1,13 @@
 
-c_repository_root = "/n/home04/kbayer/files/users"
-#c_repository_root = "/var/www/ccfa/data/files/users"
-#c_repository_root = "/aux/ccfa/upload"
+c_repository_root = "/var/www/ccfa/data/files/users"
+
+class email(object):
+    default_from_addr = "ccfa.mibc@gmail.com"
+    default_subject   = "A note from the CCFA MIBC"
+    default_smtp_serv = "localhost"
+
+class ldap(object):
+    url     = "dc2-rc"
+    bind_dn = "CN=clusterldap,OU=Unmanaged Service Accounts,DC=rc,DC=domain"
+    bind_pw = r'p$e9e!A2'
+    
