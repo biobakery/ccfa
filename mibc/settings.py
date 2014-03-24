913ea6fd69d7017080a686d886d4cd2f527da938
Randall Schwager <schwager@hsph.harvard.edu>
2014-03-24 14:06:56 +0000

    feature: user defined config files

--- mibc/settings.py
+++ mibc/settings.py
@@ -1,5 +1,7 @@
+import os
 
-c_repository_root = "/var/www/ccfa/data/files/users"
+c_repository_root = "/vagrant/data/ccfa/users"
+settings_env_var  = "MIBC_SETTINGS_FILE"
 
 class email(object):
     default_from_addr = "ccfa.mibc@gmail.com"
@@ -9,8 +11,38 @@ class email(object):
 class ldap(object):
     url         = "ldaps://dc2-rc/"
     bind_dn     = "CN=clusterldap,OU=Unmanaged Service Accounts,DC=rc,DC=domain"
-    bind_pw     = r'p$e9e!A2'
+    bind_pw     = ""
     search_base = "DC=rc,DC=domain"
     
 class users(object):
     ignored = ['admin']
+
+
+# Pay no attention to that man behind the curtain
+
+_settings_file = ""
+if settings_env_var in os.environ:
+    _settings_file = os.environ[settings_env_var]
+else:
+    # please go from global to local in this tuple literal as the last
+    # item, if it exists, is used as the settings file
+    for _p in ("/etc/mibc/settings.py", "settings.py"):
+        if os.path.exists(_p):
+            _settings_file = _p
+
+if _settings_file:
+    import imp
+    _mod = imp.load_source("settings", _settings_file)
+    globals().update([
+        ( _s, getattr(_mod,_s) )
+        for _s in dir(_mod)
+        if not _s.startswith('__')
+    ])
+
+try:
+    with open("/etc/ldap.conf") as f:
+        for match in re.finditer(r'\s*bindpw\s+(\S+)', f.read()):
+            # catch the last bindpw in the ldap.conf
+            ldap.bind_pw = match.group(1)
+except:
+    pass
