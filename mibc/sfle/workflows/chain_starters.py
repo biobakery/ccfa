ff06a3b8ad68ed49d949bd49388b287022809adc
Randall Schwager <schwager@hsph.harvard.edu>
2014-04-11 11:53:05 -0400

    fix: catch files with no extension

--- mibc/sfle/workflows/chain_starters.py
+++ mibc/sfle/workflows/chain_starters.py
@@ -6,6 +6,7 @@ def cat(env, infiles_list, guess_from):
         return env.chain("zcat", start=infiles_list, verbose=True)
     elif min_file_type == 'bzip2':
         return env.chain("bzcat", start=infiles_list, verbose=True)
-    else:
-        return env.chain("cat", start=infiles_list, verbose=True)
+
+    # if it's completely unrecognized, just return cat
+    return env.chain("cat", start=infiles_list, verbose=True)
             
