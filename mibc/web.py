27a65ba443a28a15c82430a2f8646fdd6a9b5afd
Randall Schwager <schwager@hsph.harvard.edu>
2014-04-14 20:30:02 +0000

    first workding draft of web api

--- mibc/web.py
+++ mibc/web.py
@@ -1,17 +1,21 @@
 
 import sys
+from functools import wraps
 
 from bottle import (
     run,
+    get,
     abort,
-    route,
     request
 )
 
 from . import (
     settings,
+    models,
     util
 )
+
+repo = models.Repository()
 
 def authentication_required(f):
     @wraps(f)
@@ -19,13 +23,32 @@ def authentication_required(f):
         if not request.headers.get("Authorization", "").startswith("Basic "):
             abort(401, 'Auth please!')
         return f(*args, **kwargs)
+    return wrapper
             
-            
-
-@route('/:#.*#')
+@get('/')
+@get('//')
 @authentication_required
-def debug():
-    return util.serialize(dict(request.headers))
+def root_get():
+    return util.serialize({ 'users': repo.users.all() })
+
+@get('/<name>')
+@get('//<name>')
+def user_get(name):
+    user = repo.users[name]
+    if user.exists():
+        return util.serialize(user)
+    else:
+        abort(404, 'User not found')
+
+@get('/<username>/<projectname>')
+@get('//<username>/<projectname>')
+def project_get(username, projectname):
+    project = repo.users[username].projects[projectname]
+    if project.exists():
+        return util.serialize(project)
+    else:
+        abort(404, 'Project not found')
+
 
 def main():
     run(host=settings.web.host, port=settings.web.port, 
