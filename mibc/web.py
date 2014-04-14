0f7ca7fbddcdfbd957f8d6480039ff46a7301e6e
Randall Schwager <schwager@hsph.harvard.edu>
2014-04-14 21:50:49 +0000

    add web API to EFO checking

--- mibc/web.py
+++ mibc/web.py
@@ -1,6 +1,7 @@
 
 import sys
 from functools import wraps
+from itertools import izip, chain
 
 from bottle import (
     run,
@@ -11,8 +12,10 @@ from bottle import (
 
 from . import (
     settings,
+    validate,
     models,
-    util
+    util,
+    efo
 )
 
 repo = models.Repository()
@@ -25,6 +28,14 @@ def authentication_required(f):
         return f(*args, **kwargs)
     return wrapper
             
+def project_or_404(username, projectname):
+    project = repo.users[username].projects[projectname]
+    if not project.exists():
+        abort(404, "Project not found")
+    else:
+        return project
+
+
 @get('/')
 @get('//')
 @authentication_required
@@ -43,11 +54,38 @@ def user_get(name):
 @get('/<username>/<projectname>')
 @get('//<username>/<projectname>')
 def project_get(username, projectname):
-    project = repo.users[username].projects[projectname]
-    if project.exists():
-        return util.serialize(project)
-    else:
-        abort(404, 'Project not found')
+    project = project_or_404(username, projectname)
+    return util.serialize(project)
+
+
+
+@get('/<username>/<projectname>/validate')
+@get('//<username>/<projectname>/validate')
+def validate_get(username, projectname):
+    project = project_or_404(username, projectname)
+    validation_results = validate.validate(project)
+    return util.serialize(validation_results)
+
+@get('/<username>/<projectname>/mapvalidate')
+@get('//<username>/<projectname>/mapvalidate')
+def mapvalidate_get(username, projectname):
+    project = project_or_404(username, projectname)
+    
+    to_validate = list()
+    for line_idx, line in enumerate(chain(
+            (project.map_headers,), project.map)):
+        guesses = enumerate(efo.guess(*line).items())
+        to_validate.extend(
+            [ ((line_idx, col_idx), efo_id)
+              for col_idx, (efo_id, guess) in guesses 
+              if guess is True]
+        )
+
+    coords, efo_ids = izip(*to_validate)
+    validation_results = zip(coords,efo.parallel_validate(*efo_ids).items())
+
+    return util.serialize(validation_results)
+        
 
 
 def main():
