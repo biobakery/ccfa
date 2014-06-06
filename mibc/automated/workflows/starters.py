8ce3431e3948adef3efbf61b482667f85094ff7d
Randall Schwager <schwager@hsph.harvard.edu>
2014-06-06 11:59:52 +0000

    this should be default anyway

--- mibc/automated/workflows/starters.py
+++ mibc/automated/workflows/starters.py
@@ -1,6 +1,9 @@
 import mimetypes
 
-def cat(infiles_list, guess_from):
+def cat(infiles_list, guess_from=None):
+    if not guess_from:
+        guess_from = infiles_list[0]
+
     maj_file_type, min_file_type = mimetypes.guess_type(guess_from)
     if min_file_type == 'gzip':
         return "zcat " + " ".join(infiles_list)
