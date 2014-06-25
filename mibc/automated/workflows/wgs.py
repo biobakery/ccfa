cbf31fdb45bdaa0c80c8859a85b2248c3388e79c
Randall Schwager <schwager@hsph.harvard.edu>
2014-06-25 11:35:09 +0000

    add humann workflow

--- mibc/automated/workflows/wgs.py
+++ mibc/automated/workflows/wgs.py
@@ -1,3 +1,5 @@
+import os.path
+
 from ... import (
     settings
 )
@@ -14,6 +16,33 @@ from ..util import (
 from . import (
     starters
 )
+
+def humann(infiles_list, workdir):
+    
+    _join = lambda f, label: os.path.join(workdir, "output", f+label)
+    targets = []
+    for infile in infiles_list:
+        targets.extend([
+            _join(infile, "_04a-hit-keg-mpm-cop-nul-nve-nve-xpe.txt"),
+            _join(infile, "_04a-hit-keg-mpt-cop-nul-nve-nve-xpe.txt"),
+            _join(infile, "_04b-hit-keg-mpm-cop-nul-nve-nve.txt"),
+            _join(infile, "_04b-hit-keg-mpt-cop-nul-nve-nve.txt"),
+        ])
+
+    humann_input_dir = os.path.join(workdir, "input")
+
+    return {
+        "name": "humann:"+targets[0],
+        "file_dep": infiles_list,
+        "targets": targets,
+        "actions": [
+            "cd "+workdir+"; humann_init.py",
+            "rm "+os.path.join(humann_input_dir, "*"),
+            "ln -s %s %s"%(" ".join(infiles_list), humann_input_dir),
+            "cd "+workdir+"; scons"
+        ]
+    }
+        
 
 def metaphlan2(files_list, **opts):
     """Workflow to transform WGS sequences to OTU tables, now with doit
