a39b894ad6bf6bbbde9d662a15bba733c70f5b1c
Human Microbiome Project phase 2 portal <ibdmdb@ibdmdb-int2.broadinstitute.org>
2014-08-29 12:56:31 -0400

    Addings HMP2 hooks scripts into new 'hooks' branch.

--- mibc/tm/hooks/post_pipeline_failure.sh
+++ mibc/tm/hooks/post_pipeline_failure.sh
@@ -4,3 +4,7 @@
 ##
 
 #echo "post_pipeline_failure.sh"
+mail -s 'hmp2 pipeline failure' kbayer@broadinstitute.org <<EOF
+this is the mail body
+need a pipeline name here!
+EOF
--- mibc/tm/hooks/post_task_success.sh
+++ mibc/tm/hooks/post_task_success.sh
@@ -4,3 +4,35 @@
 ##
 
 #echo "post_task_success.sh"
+
+# for HMP2, successful products from all workflows EXCEPT human workflows (kneed/bmtagger etc)
+# should be linked into separate directory structure (made available to webserver to serve)
+
+HMP2_PROCESSING_DIR=/seq/ibdmdb/processing
+
+# add link exposing job products to webserver
+for file in `echo "${TaskProducts}"`
+do
+
+  # if file doesn't exist; skip it
+  if [ ! -f "${file}" ]; then
+    echo "Warning: Product file +${file}+ doesn't exist."
+    continue;
+  fi
+
+  # strip out current 3 prefix segments
+  linkfile=`echo "${file}" | cut -d/ -f5-`
+  linkfile="${HMP2_PROCESSING_DIR}"/"${linkfile}"
+  #echo "file: ${file}"
+  #echo "linkfile: ${linkfile}"
+
+  # if link's directory doesn't exist, make it
+  linkdir=`dirname "${linkfile}"`
+  if [ ! -d "${linkdir}" ]; then
+    #echo "creating dir: ${linkdir}"
+    mkdir -p "${linkdir}"
+  fi
+
+  ln -s "${file}" "${linkfile}"
+
+done
