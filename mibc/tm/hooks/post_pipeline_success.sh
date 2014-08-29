f5f4f10231de7cd419998275d2ab849837e208a4
Human Microbiome Project phase 2 portal <ibdmdb@ibdmdb-int2.broadinstitute.org>
2014-08-29 15:07:49 -0400

    fix up email for success.

--- mibc/tm/hooks/post_pipeline_success.sh
+++ mibc/tm/hooks/post_pipeline_success.sh
@@ -4,3 +4,18 @@
 ##
 
 #echo "post_pipeline_success.sh"
+
+if [ -n ${PipelineName} ]; then
+
+  mail -s "hmp2 pipeline ${PipelineName} success" kbayer@broadinstitute.org <<EOF
+no body.
+EOF
+
+else
+
+  mail -s 'hmp2 pipeline success' kbayer@broadinstitute.org <<EOF
+no pipeline name found!
+EOF
+
+fi
+
