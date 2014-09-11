e57b3eab3a7feaeee7aea7641f66f2f3dec84027
Human Microbiome Project phase 2 portal <ibdmdb@ibdmdb-int2.broadinstitute.org>
2014-09-11 13:26:56 -0400

    handle breadcrumbs output files (not specifically in products)

--- mibc/tm/hooks/post_task_success.sh
+++ mibc/tm/hooks/post_task_success.sh
@@ -10,29 +10,46 @@
 
 HMP2_PROCESSING_DIR=/seq/ibdmdb/processing
 
-# add link exposing job products to webserver
-for file in `echo "${TaskProducts}"`
-do
+function link_products {
+  
+  # add link exposing job products to webserver
+  for file in `echo "${TaskProducts}"`
+  do
+  
+    # if file doesn't exist; skip it
+    if [ ! -f "${file}" ]; then
+      echo "Warning: Product file +${file}+ doesn't exist."
+      continue;
+    fi
+  
+    # strip out current 3 prefix segments
+    linkfile=`echo "${file}" | cut -d/ -f5-`
+    linkfile="${HMP2_PROCESSING_DIR}"/"${linkfile}"
+    #echo "file: ${file}"
+    #echo "linkfile: ${linkfile}"
+  
+    # if link's directory doesn't exist, make it
+    linkdir=`dirname "${linkfile}"`
+    if [ ! -d "${linkdir}" ]; then
+      #echo "creating dir: ${linkdir}"
+      mkdir -p "${linkdir}"
+    fi
+  
+    ln -s "${file}" "${linkfile}"
+  
+  done
+  
+}
 
-  # if file doesn't exist; skip it
-  if [ ! -f "${file}" ]; then
-    echo "Warning: Product file +${file}+ doesn't exist."
-    continue;
+task=`echo ${TaskName} | sed 's/:.*$//'`
+if [[ "${task}" == "breadcrumbs_pcoa_plot" ]]; then
+  set
+  if [[ -n ${TaskOutputDirectory} ]]; then
+    TaskProducts="${TaskOutputDirectory}otu_table_merged.biom.pcl_pcoa_plot-BarcodeSequence.png ${TaskOutputDirectory}otu_table_merged.biom.pcl_pcoa_plot-LinkerPrimerSequence.png ${TaskOutputDirectory}otu_table_merged.biom.pcl_pcoa_plot-DOB.png ${TaskOutputDirectory}otu_table_merged.biom.pcl_pcoa_plot-Treatment.png ${TaskOutputDirectory}otu_table_merged.biom.pcl_pcoa_plot-Description.png ${TaskOutputDirectory}otu_table_merged.biom.pcl_pcoa_plot-sample.png"
   fi
+fi
 
-  # strip out current 3 prefix segments
-  linkfile=`echo "${file}" | cut -d/ -f5-`
-  linkfile="${HMP2_PROCESSING_DIR}"/"${linkfile}"
-  #echo "file: ${file}"
-  #echo "linkfile: ${linkfile}"
+link_products
 
-  # if link's directory doesn't exist, make it
-  linkdir=`dirname "${linkfile}"`
-  if [ ! -d "${linkdir}" ]; then
-    #echo "creating dir: ${linkdir}"
-    mkdir -p "${linkdir}"
-  fi
 
-  ln -s "${file}" "${linkfile}"
-
-done
+ 
