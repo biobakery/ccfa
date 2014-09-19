d6db68326d53c89361571a4b784e9c5fe8fb70c7
Human Microbiome Project phase 2 portal <ibdmdb@ibdmdb-int2.broadinstitute.org>
2014-09-19 10:41:42 -0400

    Add verbose flag to all pickle scripts (echos the command to the logs)

--- mibc/tm/tasks.py
+++ mibc/tm/tasks.py
@@ -298,7 +298,7 @@ cat {pickle}
 echo "-- end picklescript script--" 
 echo ""
 echo "-- task cmd output --" 
-/usr/bin/env time -p {pickle} 
+/usr/bin/env time -p {pickle} -v 
 cmd_exit=`echo $?`
 echo "-- end task cmd output --" 
 date 
@@ -340,7 +340,7 @@ class LSFTask(Task):
 #BSUB {CLUSTER_QUEUE}
 #BSUB -o {OUTPUT}
 source {SOURCE_PATH}
-{picklescript} -r
+{picklescript} -v
 """     .format(CLUSTER_QUEUE=globals.config['CLUSTER_QUEUE'],
                        CLUSTER_JOB=globals.config['CLUSTER_JOB'],
                        SOURCE_PATH=globals.config['SOURCE_PATH'],
